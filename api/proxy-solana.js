11b4072a26b39e2147d0a4ea062a6a6b60e02f4c
accykiwi <scottkpilkington@gmail.com>
2025-04-28 14:10:51 -0700

    Update proxy-solana.js

--- api/proxy-solana.js
+++ api/proxy-solana.js
@@ -1,6 +1,6 @@
 // /api/proxy-solana.js
 
-export default async function handler(req, res) {
+export default async function handler(req, res) { 
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
